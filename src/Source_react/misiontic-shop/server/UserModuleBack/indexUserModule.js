ed4025616edf873295e1acee5f67ac8f22301f05
juanespp <jpiedrahita92@hotmail.com>
2021-10-26 13:02:26 -0500

    Actualizacion Backend Usuarios

--- src/Source_react/misiontic-shop/server/UserModuleBack/indexUserModule.js
+++ src/Source_react/misiontic-shop/server/UserModuleBack/indexUserModule.js
@@ -20,6 +20,12 @@ app.get("/get-user", async(request, response) => {
     response.json({data:rows});
 })
 
+app.get("/get-user", async (req,res) =>{
+    const correo= req.query.correo;
+    const [rows, fields] = await connection.execute(`SELECT * FROM usuarios where correo='${correo}'`);
+    console.log("Hola")
+    res.json(rows[0]) 
+})
 
 //Agregar
 app.post("/add-user", async (req, res) =>{
@@ -28,21 +34,35 @@ app.post("/add-user", async (req, res) =>{
     const nombre_usuario=user.nombre_usuario;
     const rol=user.rol;
     const estado=user.estado;
-    await connection.execute(`INSERT INTO usuarios (id, nombre_usuario, rol, estado) VALUES (${id},'${nombre_usuario}','${rol}', '${estado}') `);
+    const correo=user.correo;
+    await connection.execute(`INSERT INTO usuarios (id, nombre_usuario, rol, estado) VALUES (${id},'${nombre_usuario}','${rol}', '${estado}','${correo}') `);
     console.log(user.name)
     res.json(user);
 })
 
 // Actualizar
-app.put("/update-user", (req, res) =>{
+app.put("/update-user", async (req, res) =>{
     const user = req.body;
+    const id= user.id;
+    const nombre_usuario=user.nombre_usuario;
+    const rol=user.rol;
+    const estado=user.estado;
+    const correo=user.correo;
+    await connection.execute(`UPDATE usuarios SET rol= '${rol}' WHERE id= ${id}`);
+    await connection.execute(`UPDATE usuarios SET  estado = '${estado}' WHERE id= ${id}`);
     console.log(user.name)
     res.json(user);
 })
 
 // Borrar
-app.delete("/delete-user", (req, res) =>{
+app.delete("/delete-user", async (req, res) =>{
     const user = req.body;
+    const id= user.id;
+    const nombre_usuario=user.nombre_usuario;
+    const rol=user.rol;
+    const estado=user.estado;
+    const correo=user.correo;
+    await connection.execute(`DELETE FROM usuarios WHERE id = ${id} `);
     console.log(user.name)
     res.json(user);
 })
